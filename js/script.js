2f58c6684c311fec511acbf26232bca0b73d18cb
wolf2234 <viktoral9599@gmail.com>
2024-05-28 16:44:08 +0300

    this commit adds js to info block.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,46 @@ hamMenu.addEventListener('click', () => {
     offScreenMenu.classList.toggle('active');
     body.classList.toggle('lock');
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const infoLinks = document.querySelector('.info__links');
+    const infoContent = document.querySelector('.info__content');
+    const infoLinksChildren = infoLinks.children;
+    const infoContentChildren = infoContent.children;
+
+    for (let infoLink of infoLinksChildren) {
+        let infoLinkAttr = infoLink.dataset.infoAttr;
+        if (infoLinkAttr == '1') {
+            infoLink.classList.add('active');
+            for (let infoContent of infoContentChildren) {
+                let infoContentAttr = infoContent.dataset.infoAttr;
+                if (infoLinkAttr === infoContentAttr) {
+                    infoContent.classList.add('active');
+                }
+            }
+        }
+    }
+
+    for (let infoLink of infoLinksChildren) {
+        infoLink.addEventListener("click", function(link) {
+            let infoLinkAttr = infoLink.dataset.infoAttr;
+            for (let infoLinkChild of infoLinksChildren) {
+                infoLinkChild.classList.remove('active');
+            }
+            for (let infoContentChild of infoContentChildren) {
+                infoContentChild.classList.remove('active');
+            }
+            infoLink.classList.add('active');
+            for (let infoContent of infoContentChildren) {
+                let infoContentAttr = infoContent.dataset.infoAttr;
+                if (infoLinkAttr === infoContentAttr) {
+                    infoContent.classList.add('active');
+                }
+            }
+        });
+    }
+    
+});
