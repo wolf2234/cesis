9096ebad22ad1601fff1258090ccb5030c4e0439
wolf2234 <viktoral9599@gmail.com>
2024-06-04 14:42:19 +0300

    this commit adds slider for block People Loves Césis.

--- js/jq-script.js
+++ js/jq-script.js
@@ -7,4 +7,12 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 2000,
     });
+    $('.slider-love').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        Infinity: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 });
