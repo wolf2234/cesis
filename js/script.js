487100cc0e22590ed12bd8d5c592c616cb8d3e71
wolf2234 <viktoral9599@gmail.com>
2024-06-09 02:59:39 +1400

    this commit finishes the site CESIS, before adaptation for mobile.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const hamMenu = document.querySelector(".burger");
     const offScreenMenu = document.querySelector(".menu");
     const body = document.querySelector('.body');
     const videos = document.querySelectorAll('.video__item');
     const videoButtons = document.querySelectorAll('.video_button');
+    const plansButtons = document.querySelectorAll('.plans__button');
     const formInputs = document.querySelectorAll('.form__input');
+    const selects = document.querySelectorAll('.select');
+    const selectsItems = document.querySelectorAll('.select__item');
+
+
+    plansButtons.forEach(function (plansButton) {
+        plansButton.addEventListener('mouseover', function (button) {
+            let parent = plansButton.closest('.plans__item');
+            let price = parent.querySelector('.plans__price');
+            price.classList.add('active');
+        });
+        plansButton.addEventListener('mouseout', function (button) {
+            let parent = plansButton.closest('.plans__item');
+            let price = parent.querySelector('.plans__price');
+            price.classList.remove('active');
+        });
+    });
+
+
+    selects.forEach(function (select) {
+        select.addEventListener('click', function (option) {
+            select.classList.toggle('is-active');
+        });
+    });
+
+    selectsItems.forEach(function (selectItem) {
+        selectItem.addEventListener('click', function (selectOption) {
+            let text = selectItem.innerText;
+            let currentText = selectItem.closest('.select').querySelector('.select__current');
+            currentText.innerText = text;
+        });
+    });
 
     formInputs.forEach(function (formInput) {
         formInput.addEventListener('change', function (element) {
